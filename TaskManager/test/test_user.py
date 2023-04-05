92bab41f5ef9e712a311f8cd5c37d8b17e4572dc
https://github.com/AndreyPanda <andrey.pankin.nch@gmail.com>
2023-04-05 12:55:54 +0300

    Practice 6.7 (API testing) is partially done. Added tests for retrieve, update, delete.

--- TaskManager/test/test_user.py
+++ TaskManager/test/test_user.py
@@ -21,53 +21,34 @@ class TestUserViewSet(TestViewSetBase):
         expected_response = self.expected_details(user, self.user_attributes)
         assert user == expected_response
 
-    # def test_list(self):
-    #     user_2_attributes = {
-    #         'first_name': 'Ivan',
-    #         'last_name': 'Ivanov',
-    #         'email': 'test-admin@test.com',
-    #         'role': 'admin',
-    #     }
-    #     user_2 = self.create_api_user(user_2_attributes)
-    #     user_2_attributes['id'] = user_2.id
-    #
-    #     response = self.list()
-    #     self.user_attributes['id'] = self.user.id
-    #     expected_response = [self.user_attributes, user_2_attributes]
-    #
-    #     assert response.status_code == HTTPStatus.OK, response.content
-    #     assert response.json() == expected_response
-    #
-    # def test_retrieve(self):
-    #     response = self.retrieve(key=self.user_attributes['id'])
-    #     assert response.json() == self.user_attributes
-    #
-    # def test_update(self):
-    #     data = self.user_attributes.copy()
-    #     data['first_name'] = 'Maria'
-    #     del data['id']
-    #
-    #     response = self.update(
-    #         key=self.user.id,
-    #         data=data,
-    #     )
-    #
-    #     assert response.status_code == HTTPStatus.OK, response.content
-    #     assert response.json()['first_name'] == 'Maria'
-    #
-    # def test_delete(self):
-    #     some_user_attributes = {
-    #         'first_name': 'Godzilla',
-    #         'last_name': 'Petrov',
-    #         'email': 'godzilla@gmail.com',
-    #         'role': 'manager',
-    #     }
-    #     some_user = self.create_api_user(some_user_attributes)
-    #
-    #     response = self.delete(key=some_user.id)
-    #     response_list = self.list()
-    #     self.user_attributes['id'] = self.user.id
-    #     expected_response_list = [self.user_attributes]
-    #
-    #     assert response.status_code == HTTPStatus.NO_CONTENT, response.content
-    #     assert response_list.json() == expected_response_list
+    def test_list(self):
+        user_2 = self.create_api_user(self.user_attributes)
+        response = self.list()
+        expected_response = [
+            self.expected_details({'id': self.user.id}, self.setup_user_attributes),
+            self.expected_details({'id': user_2.id}, self.user_attributes),
+        ]
+        assert response.status_code == HTTPStatus.OK, response.content
+        assert response.json() == expected_response
+
+    def test_retrieve(self):
+        response = self.retrieve(key=self.user.id)
+        assert response.json() == self.expected_details({'id': self.user.id}, self.setup_user_attributes)
+
+    def test_update(self):
+        data = self.user_attributes.copy()
+        data['first_name'] = 'Maria'
+        response = self.update(
+            key=self.user.id,
+            data=data,
+        )
+        assert response.status_code == HTTPStatus.OK, response.content
+        assert response.json()['first_name'] == 'Maria'
+
+    def test_delete(self):
+        some_user = self.create_api_user(self.user_attributes)
+        response = self.delete(key=some_user.id)
+        response_list = self.list()
+        expected_response_list = self.expected_details({'id': self.user.id}, self.setup_user_attributes)
+        assert response.status_code == HTTPStatus.NO_CONTENT, response.content
+        assert response_list.json() == [expected_response_list]
