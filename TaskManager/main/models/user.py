7896a33b53d011a4c8cd8eddc9bca48e63d51376
https://github.com/AndreyPanda <andrey.pankin.nch@gmail.com>
2023-04-08 10:42:39 +0300

    Practice 7.2 (User model update: two fields are added) is completed.

--- TaskManager/main/models/user.py
+++ TaskManager/main/models/user.py
@@ -14,3 +14,5 @@ class User(AbstractUser):
         max_length=255, default=Roles.DEVELOPER, choices=Roles.choices
     )
     email = models.EmailField()
+    date_of_birth = models.DateField(null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
