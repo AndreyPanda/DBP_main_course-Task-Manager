afa477ed79378f98fb5b06ec76a0906bb3c1e5be
https://github.com/AndreyPanda <andrey.pankin.nch@gmail.com>
2023-05-10 07:41:29 +0300

    Feature 11.2 (Model update) is completed

--- TaskManager/main/models/user.py
+++ TaskManager/main/models/user.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from main.services.storage_backends import public_storage
 
 
 class User(AbstractUser):
@@ -16,3 +17,4 @@ class User(AbstractUser):
     email = models.EmailField()
     date_of_birth = models.DateField(null=True, blank=True)
     phone = models.CharField(max_length=20, null=True, blank=True)
+    avatar_picture = models.ImageField(null=True, storage=public_storage)
