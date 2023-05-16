fd3af93810e8125450c5571ab585a397bf69306b
https://github.com/AndreyPanda <andrey.pankin.nch@gmail.com>
2023-05-16 08:34:27 +0300

    Feature 14.4 (Celery additional adjustment) is completed.

--- TaskManager/task_manager/celery.py
+++ TaskManager/task_manager/celery.py
@@ -1,8 +1,18 @@
-import os
 from celery import Celery
+from celery.signals import before_task_publish
+from celery import states
+from typing import Any
 
 app = Celery("task_manager")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 if __name__ == "__main__":
     app.start()
+
+
+@before_task_publish.connect
+def update_sent_state(sender: str = None, headers: dict = None, **kwargs: Any) -> None:
+    task = app.tasks.get(sender)
+    backend = task.backend if task else app.backend
+
+    backend.store_result(headers["id"], None, states.STARTED)
